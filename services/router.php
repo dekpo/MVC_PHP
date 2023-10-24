2b088901068af3ccd97a8dd2fa59b8972e8b60fd
Dekpo Yologaza <dekpo@me.com>
2023-10-24 10:09:34 +0200

    Added Bootsrap and worked layout

--- services/router.php
+++ services/router.php
@@ -3,5 +3,5 @@
 // On utilise page pour trouver le controlleur 
 // Sinon la page c'est home et le controlleur c'est 
 // controller_home.php (logique)
-$getPage = strtolower($_GET['page']);
+$getPage = isset($_GET['page']) ? strtolower($_GET['page']) : "";
 $page = isset($getPage) && file_exists("./controllers/controller_".$getPage.".php") ? $getPage : array_key_first(CONFIG_ROUTES);
