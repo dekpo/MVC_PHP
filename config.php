220d3910201e4a1cccf35753db17fdec02d0ff55
Dekpo Yologaza <dekpo@me.com>
2023-10-24 09:39:46 +0200

    Refactor of the router

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ define("CONFIG_ROUTES",[
 const DB_HOST = "localhost";
 const DB_NAME = "my_mvc_bdd";
 const DB_USER = "root";
-const DB_PASS = "";
+const DB_PASS = "root";
 function connectDB(): PDO{
     $db = new PDO('mysql:host='.DB_HOST.';port=3333;dbname='.DB_NAME.';charset=utf8',DB_USER,DB_PASS);
     return $db;
--- controllers/controller_home.php
+++ controllers/controller_home.php
@@ -1,8 +1,9 @@
 <?php
 $db = connectDB();
-$sql = $db->prepare("SELECT * FROM picture");
-$sql->execute();
-$results = $sql->fetchAll(PDO::FETCH_ASSOC);
+// $sql = $db->prepare("SELECT * FROM picture");
+// $sql->execute();
+// $results = $sql->fetchAll(PDO::FETCH_ASSOC);
+//var_dump($db);
 
 // --- la vue
 include "./views/layout.phtml";
