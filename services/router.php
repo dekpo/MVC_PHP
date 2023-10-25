1ec21f5b590243c0aa48ce298b76b675b44cbe26
Dekpo Yologaza <dekpo@me.com>
2023-10-25 10:03:49 +0200

    Added some fex changes...

--- services/router.php
+++ services/router.php
@@ -4,4 +4,4 @@
 // Sinon la page c'est home et le controlleur c'est 
 // controller_home.php (logique)
 $getPage = isset($_GET['page']) ? strtolower($_GET['page']) : "";
-$page = isset($getPage) && file_exists("./controllers/controller_".$getPage.".php") ? $getPage : array_key_first(CONFIG_ROUTES);
+$page = file_exists("./controllers/controller_" . $getPage . ".php") ? $getPage : array_key_first(CONFIG_ROUTES);
